e1be0369653e36b879a771b5b5a49fc679adfddf
guoxudong01 <guoxudong01@corp.netease.com>
2022-07-04 09:34:40 +0800

    update

--- main.cpp
+++ main.cpp
@@ -6,21 +6,35 @@
 #include "state/stateb.h"
 
 #include <memory>
+#include <vector>
+#include <iostream>
+#include <map>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    auto ctx    = std::make_shared<Context>();
-    auto statea = std::make_shared<StateA>();
-    ctx->SetState(statea);
+    std::map<int, std::string> mp;
+    for(int i = 0; i < 10; i++)
+    {
+        mp.insert(std::pair<int, std::string>(i, std::to_string(i)));
+    }
 
-    auto timer = new QTimer;
-    QObject::connect(timer, &QTimer::timeout, [&](){
-        ctx->Handle();
-    });
+    for(auto itr = mp.begin(); itr != mp.end();)
+    {
+        if((*itr).first == 5)
+        {
+            itr = mp.erase(itr);
+        }
+        else
+        {
+            itr++;
+        }
+    }
 
-    timer->start(2000);
-
+    for(auto& index : mp)
+    {
+        std::cout << index.first << index.second << std::endl;
+    }
     return a.exec();
 }
